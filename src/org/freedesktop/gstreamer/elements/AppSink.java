62f65a279e77c2f61846911c32d70534d6b07ed1
christophe <christophe.lafolet@laposte.net>
2016-07-23 16:57:24 +0200

    AppSink : newPreroll and newSample shall return a FlowReturn

--- src/org/freedesktop/gstreamer/elements/AppSink.java
+++ src/org/freedesktop/gstreamer/elements/AppSink.java
@@ -25,6 +25,7 @@ package org.freedesktop.gstreamer.elements;
 
 import org.freedesktop.gstreamer.Buffer;
 import org.freedesktop.gstreamer.Caps;
+import org.freedesktop.gstreamer.FlowReturn;
 import org.freedesktop.gstreamer.Sample;
 import org.freedesktop.gstreamer.lowlevel.AppAPI;
 import org.freedesktop.gstreamer.lowlevel.GstAPI.GstCallback;
@@ -170,7 +171,7 @@ public class AppSink extends BaseSink {
          *
          * @param elem
          */
-        public void newBuffer(AppSink elem);
+        public FlowReturn newBuffer(AppSink elem);
     }
     /**
      * Adds a listener for the <code>new-buffer</code> signal. If a blocking
@@ -183,8 +184,8 @@ public class AppSink extends BaseSink {
     public void connect(final NEW_SAMPLE listener) {
         connect(NEW_SAMPLE.class, listener, new GstCallback() {
             @SuppressWarnings("unused")
-            public void callback(AppSink elem) {
-                listener.newBuffer(elem);
+            public FlowReturn callback(AppSink elem) {
+                return listener.newBuffer(elem);
             }
         });
     }
@@ -205,7 +206,7 @@ public class AppSink extends BaseSink {
          *
          * @param elem
          */
-        public void newPreroll(AppSink elem);
+        public FlowReturn newPreroll(AppSink elem);
     }
     /**
      * Adds a listener for the <code>new-preroll</code> signal. If a blocking
@@ -218,8 +219,8 @@ public class AppSink extends BaseSink {
     public void connect(final NEW_PREROLL listener) {
         connect(NEW_PREROLL.class, listener, new GstCallback() {
             @SuppressWarnings("unused")
-            public void callback(AppSink elem) {
-                listener.newPreroll(elem);
+            public FlowReturn callback(AppSink elem) {
+                return listener.newPreroll(elem);
             }
         });
     }
