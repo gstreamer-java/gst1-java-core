5768e417e1aae1feaf09128ddc2e1ef0cb5a14aa
Matthias Bläsing <mblaesing@doppel-helix.eu>
2016-10-08 13:42:04 +0200

    Fix Query API

--- src/org/freedesktop/gstreamer/lowlevel/SubtypeMapper.java
+++ src/org/freedesktop/gstreamer/lowlevel/SubtypeMapper.java
@@ -146,7 +146,7 @@ class SubtypeMapper {
             }};
             public static Class<? extends NativeObject> subtypeFor(Pointer ptr) {
                 GstQueryAPI.QueryStruct struct = new GstQueryAPI.QueryStruct(ptr);
-                QueryType type = QueryType.valueOf((Integer) struct.readField("type"));
+                QueryType type = QueryType.values()[((Integer) struct.readField("type"))];
                 Class<? extends Query> queryClass = typeMap.get(type);
                 return queryClass != null ? queryClass : Query.class;
             }
