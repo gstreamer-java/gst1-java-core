82f487ab2bcd2767a9220f2f1adbb9ec62928aff
Isaac Raño Jares <isaacrj@scioit.es>
2017-02-01 13:19:29 +0100

    Added support for request-pt-map and on-new-ssrc signals

--- src/org/freedesktop/gstreamer/elements/RtpBin.java
+++ src/org/freedesktop/gstreamer/elements/RtpBin.java
@@ -1,6 +1,10 @@
 package org.freedesktop.gstreamer.elements;
 
 import org.freedesktop.gstreamer.Bin;
+import org.freedesktop.gstreamer.Caps;
+import org.freedesktop.gstreamer.lowlevel.GstAPI.GstCallback;
+
+import com.sun.jna.Pointer;
 
 public class RtpBin extends Bin {
 	
@@ -18,7 +22,45 @@ public class RtpBin extends Bin {
     public RtpBin(Initializer init) {
 		super(init);
 	}
+
+    public static interface REQUEST_PT_MAP {
+    	
+    	public Caps onRtpMapRequest(RtpBin rtpBin, int session, int pt);
+    	
+    }
+
+    public void connect(final REQUEST_PT_MAP listener) {
+        connect(REQUEST_PT_MAP.class, listener, new GstCallback() {
+            @SuppressWarnings("unused")
+            public Caps callback(Pointer rtpBin, int session, int pt, Pointer data) {
+               return listener.onRtpMapRequest(RtpBin.this, session, pt);
+            }
+        });
+    }
+
+    public void disconnect(final REQUEST_PT_MAP listener) {
+    	disconnect(REQUEST_PT_MAP.class, listener);
+    }
     
+    public static interface ON_NEW_SSRC {
+    	
+    	public void onNewSsrc(RtpBin rtpBin, int session, int ssrc);
+    	
+    }
+
+    
+    public void connect(final ON_NEW_SSRC listener) {
+        connect(ON_NEW_SSRC.class, listener, new GstCallback() {
+            @SuppressWarnings("unused")
+            public void callback(Pointer rtpBin, int session, int ssrc, Pointer data) {
+                listener.onNewSsrc(RtpBin.this, session, ssrc);
+            }
+        });
+    }
+
+    public void disconnect(final ON_NEW_SSRC listener) {
+    	disconnect(ON_NEW_SSRC.class, listener);
+    }
     
 
 }
