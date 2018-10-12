bf13f52547c76d413f1329a13b5d6a4f306db66b
Neil C Smith <neilcsmith.net@googlemail.com>
2018-10-12 17:52:33 +0100

    Update Gst.parseLaunch behaviour, deprecate replaced methods (#113)
    * test Gst.parseBinFromDescription and Gst.parseLaunch
    
    * Update parse methods to support single elements.
    
    * Update parseLaunch tests to work with Element return; add single element test to check that a Pipeline class isn't returned.
    
    * Tidy up documentation.

--- src/org/freedesktop/gstreamer/Gst.java
+++ src/org/freedesktop/gstreamer/Gst.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright (c) 2017 Neil C Smith
+ * Copyright (c) 2018 Neil C Smith
  * Copyright (c) 2007 Wayne Meissner
  * 
  * This file is part of gstreamer-java.
@@ -213,14 +213,16 @@ public final class Gst {
      * to play the pipeline.
      * 
      * @param pipelineDescription  the command line describing the pipeline
+     * @param errors a list that any errors will be added to
      * @return a new element on success. 
-     *         If more than one toplevel element is specified by 
-     *         the pipeline_description , all elements are put into a GstPipeline, 
-     *         which than is returned. 
+     *         If more than one top-level element is specified by 
+     *         the pipeline_description , all elements are put into a Pipeline, 
+     *         which then is returned.
+     * @throws GstException if the pipeline / element could not be created
      */
-    public static Pipeline parseLaunch(String pipelineDescription, List<GError> errors) {
+    public static Element parseLaunch(String pipelineDescription, List<GError> errors) {
         Pointer[] err = { null };
-        Pipeline pipeline = GSTPARSE_API.gst_parse_launch(pipelineDescription, err);
+        Element pipeline = GSTPARSE_API.gst_parse_launch(pipelineDescription, err);
         if (pipeline == null) {
             throw new GstException(new GError(new GErrorStruct(err[0])));
         }
@@ -235,8 +237,24 @@ public final class Gst {
         }
 
         return pipeline;
-    }    
-    public static Pipeline parseLaunch(String pipelineDescription) {
+    }
+    
+    /**
+     * Create a new pipeline based on command line syntax.
+     * 
+     * Please note that you might get a return value that is not NULL even 
+     * though the error is set. 
+     * In this case there was a recoverable parsing error and you can try 
+     * to play the pipeline.
+     * 
+     * @param pipelineDescription  the command line describing the pipeline
+     * @return a new element on success. 
+     *         If more than one top-level element is specified by 
+     *         the pipeline_description , all elements are put into a Pipeline, 
+     *         which then is returned.
+     * @throws GstException if the pipeline / element could not be created
+     */
+    public static Element parseLaunch(String pipelineDescription) {
         return parseLaunch(pipelineDescription, null);
     }
     
@@ -247,11 +265,13 @@ public final class Gst {
      * An error does not mean that the pipeline could not be constructed.
      * 
      * @param pipelineDescription An array of strings containing the command line describing the pipeline.
+     * @param errors a list that any errors will be added to
      * @return a new element on success. 
+     * @throws GstException if the pipeline / element could not be created
      */
-    public static Pipeline parseLaunch(String[] pipelineDescription, List<GError> errors) {
+    public static Element parseLaunch(String[] pipelineDescription, List<GError> errors) {
         Pointer[] err = { null };
-        Pipeline pipeline = GSTPARSE_API.gst_parse_launchv(pipelineDescription, err);
+        Element pipeline = GSTPARSE_API.gst_parse_launchv(pipelineDescription, err);
         if (pipeline == null) {
             throw new GstException(new GError(new GErrorStruct(err[0])));
         }
@@ -267,7 +287,18 @@ public final class Gst {
 
         return pipeline;
     }
-    public static Pipeline parseLaunch(String[] pipelineDescription) {
+    
+    /**
+     * Create a new element based on command line syntax. 
+     * 
+     * error will contain an error message if an erroneous pipeline is specified. 
+     * An error does not mean that the pipeline could not be constructed.
+     * 
+     * @param pipelineDescription An array of strings containing the command line describing the pipeline.
+     * @return a new element on success. 
+     * @throws GstException if the pipeline / element could not be created
+     */
+    public static Element parseLaunch(String[] pipelineDescription) {
         return parseLaunch(pipelineDescription, null);
     }
     
@@ -279,7 +310,9 @@ public final class Gst {
     * 
     * @param binDescription the command line describing the bin
     * @param ghostUnlinkedPads whether to create ghost pads for the bin from any unlinked pads
+    * @param errors list that any errors will be added to
     * @return The new Bin.
+    * @throws GstException if the bin could not be created
     */
    public static Bin parseBinFromDescription(String binDescription, boolean ghostUnlinkedPads, List<GError> errors) {
         
@@ -301,6 +334,18 @@ public final class Gst {
         
         return bin;
    }
+   
+   /**
+    * Creates a bin from a text bin description. 
+    * 
+    * This function allows creation of a bin based on the syntax used in the
+    * gst-launch utillity.
+    * 
+    * @param binDescription the command line describing the bin
+    * @param ghostUnlinkedPads whether to create ghost pads for the bin from any unlinked pads
+    * @return The new Bin.
+    * @throws GstException if the bin could not be created
+    */
    public static Bin parseBinFromDescription(String binDescription, boolean ghostUnlinkedPads) {
        return parseBinFromDescription(binDescription, ghostUnlinkedPads, null);
    }
