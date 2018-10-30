4309d89cbbb547e0abf3aca3c3bd07d45e4bdfdc
a-morales <antonio@aifoundation.com>
2018-10-30 10:17:12 -0700

    clean up names for WebRTCSDPType enum

--- src/org/freedesktop/gstreamer/WebRTCSDPType.java
+++ src/org/freedesktop/gstreamer/WebRTCSDPType.java
@@ -3,10 +3,10 @@ package org.freedesktop.gstreamer;
 import org.freedesktop.gstreamer.lowlevel.IntegerEnum;
 
 public enum WebRTCSDPType implements IntegerEnum {
-    GST_WEBRTC_SDP_TYPE_OFFER(1),
-    GST_WEBRTC_SDP_TYPE_PRANSWER(2),
-    GST_WEBRTC_SDP_TYPE_ANSER(3),
-    GST_WEBRTC_SDP_TYPE_ROLLBACK(4);
+    OFFER(1),
+    PRANSWER(2),
+    ANSWER(3),
+    ROLLBACK(4);
 
     WebRTCSDPType(int value) {
         this.value = value;
